4ffe31c53b5843005900b6e45117b4c12d922419
Naningntch <64010232@kmitl.ac.th>
2023-04-21 06:40:04 +0700

    edit style.css and ActiveOrders.jsx

--- frontend/src/routes/main/Orders.jsx
+++ frontend/src/routes/main/Orders.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Authentication from "../../components/Authentication";
+import ActiveOrders from "../../components/ActiveOrders";
 
 const Orders = () => {
 	return (
 		<div>
 			<Authentication/>
+			<ActiveOrders/>
 		</div>
 	);
 }
